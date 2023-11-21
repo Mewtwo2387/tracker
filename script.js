682baa1d7b727de6c9912754b0a535b26ee790d9
mewtwo2387 <mewtwo2387@gmail.com>
2023-11-21 11:54:53 +0000

    added completed tasks only bar

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 function update(){
     for(const module of data.modules){
         moduletotal = 0
+        modulecomplete = 0
         for(const task of module.tasks){
             id = module.id + task.id
             taskpercent = ((document.getElementById('i' + id).value / document.getElementById('i' + id).max) * 100);
             modulepercent = taskpercent * task.weight;
             moduletotal += modulepercent;
+            if(modulepercent!=0){modulecomplete += task.weight}
             allpercent = modulepercent * module.credits/data.totalCredits;
             document.getElementById('b' + id).style.width = taskpercent + '%';
             document.getElementById('t' + id).innerHTML = `${taskpercent.toFixed(2)}/100% of task`
             document.getElementById('m' + id).innerHTML = `${modulepercent.toFixed(2)}/${(task.weight*100).toFixed(2)}% of module`
             document.getElementById('a' + id).innerHTML = `${allpercent.toFixed(2)}/${(task.weight*module.credits/data.totalCredits*100).toFixed(2)}% of all`
         }
+
         document.getElementById('b' + module.id).style.width = moduletotal + '%';
         moduleallpercent = moduletotal * module.credits/data.totalCredits
         document.getElementById('m' + module.id).innerHTML = `${moduletotal.toFixed(2)}/100% of module`
         document.getElementById('a' + module.id).innerHTML = `${moduleallpercent.toFixed(2)}/${(module.credits/data.totalCredits*100).toFixed(2)}% of all`
 
+        
+        if(modulecomplete==0){
+            modulecompletepercent = 0
+        }else{
+            modulecompletepercent = moduletotal / modulecomplete
+        }
+
+        document.getElementById('bc' + module.id).style.width = modulecompletepercent + '%';    
+        moduletotalall = moduletotal * module.credits/data.totalCredits
+        modulecompleteall = modulecomplete * module.credits/data.totalCredits
+        document.getElementById('mc' + module.id).innerHTML = `${moduletotal.toFixed(2)}/${(modulecomplete*100).toFixed(2)}% of module (completed tasks only)`;
+        document.getElementById('ac' + module.id).innerHTML = `${moduletotalall.toFixed(2)}/${(modulecompleteall*100).toFixed(2)}% of all (completed tasks only)`;
     }
 }
 
@@ -26,7 +41,7 @@ function load(){
     for(const module of data.modules){
         for(const task of module.tasks){
             if(task.id=='a'){
-                html += `<tr><td rowspan="${module.tasks.length}">${module.name}<br><span class="small">(${module.credits} credits - ${(module.credits/data.totalCredits*100).toFixed(2)}%)</span><div class="progress-bar"><div class="progress-bar-inner" style="width:0%" id="${'b'+module.id}"></div></div><span class="small" id="${'m'+module.id}">--/100% of module</span><br><span class="small" id="${'a'+module.id}">--/--% of all</span></td>`
+                html += `<tr><td rowspan="${module.tasks.length}">${module.name}<br><span class="small">(${module.credits} credits - ${(module.credits/data.totalCredits*100).toFixed(2)}%)</span><div class="progress-bar"><div class="progress-bar-inner" style="width:0%" id="${'b'+module.id}"></div></div><span class="small" id="${'m'+module.id}">--/100% of module</span><br><span class="small" id="${'a'+module.id}">--/--% of all</span><div class="progress-bar"><div class="progress-bar-inner" style="width:0%" id="${'bc'+module.id}"></div></div><span class="small" id="${'mc'+module.id}">--/--% of module</span><br><span class="small" id="${'ac'+module.id}">--/--% of all</span></td>`
             }else{
                 html += `<tr>`
             }
