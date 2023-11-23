0d1d2ac0ca9d5aa1c4ae6d6b20a728c7ba0074a3
mewtwo2387 <mewtwo2387@gmail.com>
2023-11-23 17:40:42 +0000

    added all data

--- data.js
+++ data.js
@@ -3,6 +3,118 @@ const data =
     "modules": [
         {
             "id": 0,
+            "name": "40001 - Computer Systems",
+            "credits": 5,
+            "tasks": [
+                {
+                    "id": 'a',
+                    "name": "CW1",
+                    "type": "cw",
+                    "maxScore": 60,
+                    "weight": 0.03
+                },
+                {
+                    "id": 'b',
+                    "name": "CW2",
+                    "type": "cw",
+                    "maxScore": 60,
+                    "weight": 0.06
+                },
+                {
+                    "id": 'c',
+                    "name": "CW3",
+                    "type": "cw",
+                    "maxScore": 60,
+                    "weight": 0.06
+                },
+                {
+                    "id": 'd',
+                    "name": "Final",
+                    "type": "test",
+                    "maxScore": 100,
+                    "weight": 0.85
+                }
+            ]
+        },
+        {
+            "id": 1,
+            "name": "40005 - Computer Architechure",
+            "credits": 5,
+            "tasks": [
+                {
+                    "id": 'a',
+                    "name": "CW1",
+                    "type": "cw",
+                    "maxScore": 100,
+                    "weight": 0.1
+                },
+                {
+                    "id": 'b',
+                    "name": "CW2",
+                    "type": "cw",
+                    "maxScore": 100,
+                    "weight": 0.1
+                },
+                {
+                    "id": 'c',
+                    "name": "Final",
+                    "type": "test",
+                    "maxScore": 100,
+                    "weight": 0.80
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "name": "40007 - Databases",
+            "credits": 5,
+            "tasks": [
+                {
+                    "id": 'a',
+                    "name": "CW1",
+                    "type": "cw",
+                    "maxScore": 100,
+                    "weight": 0.1
+                },
+                {
+                    "id": 'b',
+                    "name": "CW2",
+                    "type": "cw",
+                    "maxScore": 100,
+                    "weight": 0.1
+                },
+                {
+                    "id": 'c',
+                    "name": "Final",
+                    "type": "test",
+                    "maxScore": 100,
+                    "weight": 0.80
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "name": "40008 - Graphs and Algorithms",
+            "credits": 5,
+            "tasks": [
+                {
+                    "id": 'a',
+                    "name": "CW",
+                    "type": "cw",
+                    "maxScore": 100,
+                    "weight": 0.2
+                },
+                {
+                    "id": 'b',
+                    "name": "Final",
+                    "type": "test",
+                    "maxScore": 100,
+                    "weight": 0.8
+                }
+            ]
+        },
+        {
+            "id": 4,
             "name": "40009 - Computing Practical 1",
             "credits": 20,
             "tasks": [
@@ -24,7 +136,7 @@ const data =
                     "id": 'c',
                     "name": "Ethics",
                     "type": "project",
-                    "maxScore": 99,
+                    "maxScore": 100,
                     "weight": 0.1
                 },
                 {
@@ -45,12 +157,12 @@ const data =
                     "id": 'f',
                     "name": "Group Project",
                     "type": "project",
-                    "maxScore": 99,
+                    "maxScore": 100,
                     "weight": 0.08
                 },
                 {
                     "id": 'g',
-                    "name": "Test",
+                    "name": "C Final",
                     "type": "test",
                     "maxScore": 20,
                     "weight": 0.12
@@ -58,7 +170,91 @@ const data =
             ]
         },
         {
-            "id": 1,
+            "id": 5,
+            "name": "40016 - Calculus",
+            "credits": 5,
+            "tasks": [
+                {
+                    "id": 'a',
+                    "name": "MMT1",
+                    "type": "cw",
+                    "maxScore": 20,
+                    "weight": 0.05
+                },
+                {
+                    "id": 'b',
+                    "name": "MMT2",
+                    "type": "cw",
+                    "maxScore": 20,
+                    "weight": 0.05
+                },
+                {
+                    "id": 'c',
+                    "name": "MMT3",
+                    "type": "cw",
+                    "maxScore": 20,
+                    "weight": 0.05
+                },
+                {
+                    "id": 'd',
+                    "name": "MMT4",
+                    "type": "cw",
+                    "maxScore": 20,
+                    "weight": 0.05
+                },
+                {
+                    "id": 'e',
+                    "name": "Final",
+                    "type": "test",
+                    "maxScore": 100,
+                    "weight": 0.80
+                }
+            ]
+        },
+        {
+            "id": 6,
+            "name": "40017 - Linear Algebra",
+            "credits": 5,
+            "tasks": [
+                {
+                    "id": 'a',
+                    "name": "MMT1",
+                    "type": "cw",
+                    "maxScore": 100,
+                    "weight": 0.05
+                },
+                {
+                    "id": 'b',
+                    "name": "MMT2",
+                    "type": "cw",
+                    "maxScore": 100,
+                    "weight": 0.05
+                },
+                {
+                    "id": 'c',
+                    "name": "MMT3",
+                    "type": "cw",
+                    "maxScore": 100,
+                    "weight": 0.05
+                },
+                {
+                    "id": 'd',
+                    "name": "MMT4",
+                    "type": "cw",
+                    "maxScore": 100,
+                    "weight": 0.05
+                },
+                {
+                    "id": 'e',
+                    "name": "Final",
+                    "type": "test",
+                    "maxScore": 100,
+                    "weight": 0.80
+                }
+            ]
+        },
+        {
+            "id": 7,
             "name": "40018 - Discrete Maths, Logic, and Reasoning",
             "credits": 10,
             "tasks": [
