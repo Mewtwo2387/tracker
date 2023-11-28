4b7561634805d0a7ab4a6ae7c22bd6a63cf866ea
mewtwo2387 <mewtwo2387@gmail.com>
2023-11-28 00:57:22 +0000

    fixed cookies resetting

--- script.js
+++ script.js
@@ -119,5 +119,5 @@ function getCookie(cname) {
   }
 
 function setCookie(cname, cvalue) {
-    document.cookie = cname + "=" + cvalue + ";path=/";
+    document.cookie = cname + "=" + cvalue + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 }
